07ded293ac686d8f5e21772a378cc01a9d92382d
MrPattarapong_Phengtavee <138426033+Phattarapong26@users.noreply.github.com>
2024-02-16 21:04:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Easy from '../Componets/EasyGame/Easy';
